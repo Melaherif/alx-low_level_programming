3150e6c996c1b441954043a59014b33883c6c78a
Melaherif <melsherrif57@gamil.com>
2023-10-05 15:19:08 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * The function prints"Programming is like building a multilingual puzzle"
+ * followed by a new line and then returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return(0);
+
+
+
+}
