a7ef07b051d8a0a71c974a4845a2ac0d9074060e
Melaherif <melsherrif57@gamil.com>
2023-10-05 17:42:23 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-printf("Size of a char:%lubyte (s)\n", sizeof(char));
-printf("Size of an int:%lubyte (s)\n", sizeof(int));
-printf("Size of a long int:%lubyte (s)\n", sizeof(long int));
-printf("Size of a long long int:%lubyte (s)", sizeof(long long int));
-printf("Size of a float:%lubyte (s)", sizeof(float));
+printf("Size of a char:%lubyte(s)\n", sizeof(char));
+printf("Size of an int:%lubyte(s)\n", sizeof(int));
+printf("Size of a long int:%lubyte(s)\n", sizeof(long int));
+printf("Size of a long long int:%lubyte(s)\n", sizeof(long long int));
+printf("Size of a float:%lubyte(s)\n", sizeof(float));
 return (0);
 }
