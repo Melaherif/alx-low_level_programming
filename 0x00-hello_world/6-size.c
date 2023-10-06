49afe3489e6343060bb6cca9c2605f19e4700a0c
Melaherif <melsherrif57@gamil.com>
2023-10-05 17:15:09 -0700

    sizof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-Entrypoint
+*
+*Description: A c program that prints with sizeof function
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of a char:byte %lu\n (s)", sizeof(char));
+printf("Size of a float:byte %lu\n (s)", sizeof(float));
+printf("Size of a int:byte %lu\n (s)", sizeof(int));
+return (0);
+}
