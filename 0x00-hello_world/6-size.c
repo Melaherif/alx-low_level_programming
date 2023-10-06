6c4d4cbbf6eca63d8122c9651eaacf5b20cb16b3
Melaherif <melsherrif57@gamil.com>
2023-10-05 17:22:55 -0700

    sizei

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,9 @@
 int main(void)
 {
 printf("Size of a char:byte %lu\n (s)", sizeof(char));
+printf("Size of an int:byte %lu\n (s)", sizeof(int));
+printf("Size of a long int:byte %lu\n (s)", sizeof(long int));
+printf("Size of a long long int:byte %lu\n (s)", sizeof(long long int);
 printf("Size of a float:byte %lu\n (s)", sizeof(float));
-printf("Size of a int:byte %lu\n (s)", sizeof(int));
 return (0);
 }
