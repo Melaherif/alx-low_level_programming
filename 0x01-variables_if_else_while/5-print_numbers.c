ecfcb8b1ffadd534979864a97bf56a94bcd94e08
Melaherif <melsherrif57@gamil.com>
2023-10-09 08:09:21 -0700

    for

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main-Entrypoint
+*
+*Description: A c program that prints with for function
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 0 ; a <= 9 ; a++)
+printf("%d", a);
+printf("\n");
+return (0);
+}
