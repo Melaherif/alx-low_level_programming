48590d3e27e87e22aeb0de0e0673c77e87ba6a0a
Melaherif <melsherrif57@gamil.com>
2023-10-09 07:26:20 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main-Entrypoint
+*
+*Description: A c program that prints with putchar function
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+}
+
