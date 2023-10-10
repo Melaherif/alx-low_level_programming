94d750f91451d5a7e686fe11fe15f95596e9dfa6
Melaherif <melsherrif57@gamil.com>
2023-10-10 15:17:06 -0700

    functiin

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10- utilizes on the _putchar function to print
+*the alphabet a - z x10
+*/
+void print_alphabet_x10(void)
+{
+int i;
+char n;
+for (i = 0 ; i <= 10; i++)
+{
+for (n = 'a'; n <= 'z'; n++)
+_putchar(n);
+_putchar('\n');
+}
+}
+
+
+
