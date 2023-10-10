542fb284d617d9fe8563efafd449fd0ccc2bbba5
Melaherif <melsherrif57@gamil.com>
2023-10-10 14:08:39 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
 *main-Entrypoint
 *
-*Description: A c program that prints with puts function
+*Description: A c program that prints with putchar function
 *
 *Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
