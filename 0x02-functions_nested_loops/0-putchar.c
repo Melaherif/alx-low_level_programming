647686e96ca9104aa611c31bd8211971edd5d397
Melaherif <melsherrif57@gamil.com>
2023-10-10 04:51:53 -0700

    puts

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main-Entrypoint
+*
+*Description: A c program that prints with puts function
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("_putchar");
+return (0);
+}
