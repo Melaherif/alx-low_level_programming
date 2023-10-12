ea1600046766bdfdf0d317661d14e01b7f397678
Melaherif <melsherrif57@gamil.com>
2023-10-12 14:17:36 -0700

    Draw

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_line-draws a straight line in the terminal.
+*@n:the number of lines to draw
+*Return:empty
+*/
+void print_line(int n)
+{
+int i;
+{
+for (i <= 0; i <= n ; i++)
+{
+if (n >= 0)
+_putchar('\n');
+}
+_putchar("_");
+_putchar("\n");
+}
+}
