d5c9692381fcd2808ec188406bf05adbbb554ad8
Melaherif <melsherrif57@gamil.com>
2023-10-12 08:03:29 -0700

    0to9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_most_numbers-print the numbers since 0 up to 9 else 2 and 4
+*Return:the number since 0 up tp 9 else 2 and 0
+*/
+void print_most_numbers(void)
+{
+int i;
+{
+for (i = 0; i <= 9; i++)
+if(i != 2 && i !=4)
+_putchar(i);
+}
+_putchar('\n');
+}
