e5614f0e91d9887e772786a390a46d7dd3f10554
Melaherif <melsherrif57@gamil.com>
2023-10-12 08:36:33 -0700

    nested for

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*more_numbers- print numbers since 0 up to 14 in the 10 line
+*Return:the result numbers since 0 up to 15 in the 10 line
+*/
+void more_numbers(void)
+{
+int i;
+int n;
+for (n = 0; n <= 10; n++)
+{
+for (i = 0; i <= 14; i++)
+_putchar(i);
+_putchar('\n');
+}
+}
