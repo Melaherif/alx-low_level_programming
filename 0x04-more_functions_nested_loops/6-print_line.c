d988bffa7186369469941396913c0813b89b4e55
Melaherif <melsherrif57@gamil.com>
2023-10-12 14:23:38 -0700

    draw

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,16 @@
 void print_line(int n)
 {
 int i;
+if (n <= 0)
+{
+_putchar('\n')
+}
+else
 {
 for (i <= 0; i <= n ; i++)
 {
-if (n >= 0)
-_putchar('\n');
+_putchar('92');
 }
-_putchar("_");
 _putchar("\n");
 }
 }
