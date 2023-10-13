06533c43472a1973b84254a935a66d88ae9a90a2
Melaherif <melsherrif57@gamil.com>
2023-10-13 15:00:56 -0700

    9

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,11 +6,20 @@
 */
 void print_square(int size)
 {
-int i;
+int i, n;
+{
 if (size <= 0)
 _putchar('\n');
+}
 else
+{
+for (n = 0; n < size ; n++)
+{
 for (i = 0 ; i < size ; i++)
+{
 _putchar(35);
+}
 _putchar('\n');
+}
+}
 }
