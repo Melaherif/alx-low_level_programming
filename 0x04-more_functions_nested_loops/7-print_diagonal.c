ed0fd0b141510280d9fd0bde718e1548e568237b
Melaherif <melsherrif57@gamil.com>
2023-10-13 14:38:24 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,17 @@ void print_diagonal(int n)
 {
 int postn, space;
 if (n <= 0)
+{
 _putchar('\n');
+}
 else
 {
-for (postn = 0; postn <= n ; postn++)
+for (postn = 0; postn < n ; postn++)
 {
-for (space = 0; space <= postn ; space++)
+for (space = 0; space < postn ; space++)
+{
 _putchar(32);
+}
 _putchar(92);/*is equal to '/' char*/
 _putchar('\n');
 }
