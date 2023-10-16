4a6440e13427c6625152c84f461dbcb0edf20283
Melaherif <melsherrif57@gamil.com>
2023-10-16 15:58:17 -0700

    revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_rev-a function prints a string in reverse followed by a new line
+*@s:a para on function
+*Return:empty
+**/
+char print_rev(char *s)
+{
+int i;
+while (s[i] != '\0')
+{
+i++;
+}
+while (i--)
+{
+putchar (s[i]);
+}
+putchar ('\n');
+}
