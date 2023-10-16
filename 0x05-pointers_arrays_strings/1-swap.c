ca452f20c44717f67b7b8c5387cb2e6172b2f6b1
Melaherif <melsherrif57@gamil.com>
2023-10-16 10:00:32 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*swap_int- a function swap the para b on a
+*@a:is para swap to b
+*@b:is para swap to a
+*Return:empty
+**/
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
