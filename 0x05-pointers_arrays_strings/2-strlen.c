27ea52fe2b2fdab59fda94d98ea7b60a56828ffb
Melaherif <melsherrif57@gamil.com>
2023-10-16 13:49:00 -0700

    lenth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_strlen-a function reuturn the length of a string
+*@s:a char para on function
+*Return:the value of (i) length of a string
+**/
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+return (i);
+}
