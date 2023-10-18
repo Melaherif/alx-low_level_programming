ae35fd04633e360fb36295a1c1b0797aab10f1df
Melaherif <melsherrif57@gamil.com>
2023-10-18 15:07:19 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strcat- a function that concatenates two strings
+*@n: a para in function
+*@dest: a para in function
+*@src: a para in function
+*Return:dest value
+*/
+char *_strncat(char *dest, char *src, int n);
+{
+int i, x;
+i = 0;
+while (dest[i])
+i++;
+for (x = 0; x < n && src[x] != '\0'; x++)
+dest[i + x] = src[x];
+dest (c + i) = '\0'
+return (dest);
+}
